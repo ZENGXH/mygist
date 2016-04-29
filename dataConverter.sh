ee9a25ddec3804c53412c86c23143847103502e5
ZENGXH <xzengaf@ust.hk>
2016-04-30 01:53:49 +0800

    add scripts; add scale100

--- dataConverter.sh
+++ dataConverter.sh
@@ -1,23 +1,29 @@
 #!/bin/sh
-year="2013"
 
-# cp ../../../hko/data/$year*.tar.gz .
-tar xzf $year.tar.gz
 
-for file in $(find ./$year -name 'RAD*.REF2256') # for file in ./$year/*  # ${myhome}/SPARNN/data/2015/*
+for year in "2009" "2010" "2011" "2012" "2013" "2014"
 do
-    if [[ -f $file ]]; then
-        # copy stuff ....
-        # make_png.lin.x RAD130522024800.REF2256 256 2 1 RAD130522024800.REF2256-linear.png
-        # make_png.lin.x f 256 2 1 RAD130522024800.REF2256-linear.png
-	# echo $file
-	IFS='/' read -a myarray <<< "$file"
-	# echo "IP: ${myarray[1]}"
-        output="${myarray[2]}-linear.png"
-	echo $output
-	/project/dygroup2/xiaohui/SPARNN/data/radar2PNGUtil/make_png.nobg.lin.x $file 256 2 1 $output 
-    fi
+    # cp ../../../hko/data/$year*.tar.gz .
+    tar xzf ${year}.tar.gz
+    find ./${year} -type f -name 'RAD*.REF2256' -print0 | xargs -0 mv -t ./${year}
+    find ./${year} type d -empty -delete
+
+    for file in $(find ./$year -name 'RAD*.REF2256') # for file in ./$year/*  # ${myhome}/SPARNN/data/2015/*
+    do
+        if [[ -f $file ]]; then
+            # copy stuff ....
+            # make_png.lin.x RAD130522024800.REF2256 256 2 1 RAD130522024800.REF2256-linear.png
+            # make_png.lin.x f 256 2 1 RAD130522024800.REF2256-linear.png
+        # echo $file
+        IFS='/' read -a myarray <<< "$file"
+        echo "IP: ${myarray[2]}"
+        echo ${file}
+            output="${year}/${myarray[2]}-linear.png"
+        echo $output
+        /project/dygroup2/xiaohui/SPARNN/data/radar2PNGUtil/make_png.nobg.lin.x $file 256 2 1 $output
+        fi
+    done
+
+python imageRenameCrop.py ${year}
+
 done
-
-python imageRenameCrop.py 13
-
--- imageRenameCrop.py
+++ imageRenameCrop.py
@@ -14,22 +14,30 @@ from PIL import Image
         for ind, line in enumerate(lines):
 """
 
-listFile = "../SPARNN/totalFileList.txt"
+year = str(sys.argv[1])
 
-of = open(listFile, 'r')
-imageList = of.readlines()
-of.close()
-datapath = "../SPARNN/data/2015/"
+path = './20' + year + '/'
+nameOfFile = 'RAD*.png'
+fileList = glob.glob(path + nameOfFile)
+
+# listFile = "../SPARNN/totalFileList.txt"
+
+# of = open(listFile, 'r')
+# imageList = of.readlines()
+# of.close()
+datapath = path
 num = 1
-for ind, line in enumerate(imageList):
 
-	oldname = datapath + line.split('\n')[0]
-	newname = datapath + 'img15' + str(num).zfill(6) + '.png'
+for ind, line in enumerate(fileList):
+	oldname = line.split('\n')[0]
+	newname = datapath + 'img'+ year + str(num).zfill(6) + '.png'
 	img = Image.open(oldname)
 	img = img.crop((75,75,405,405)) ## im.crop((left, top, right, bottom))
+	img.thumbnail((100, 100))
 	img.save(newname, 'png')
 	print(oldname)
 	print(newname)
 	num += 1
+	
 	## newname = datapath + 
 	## os.rename(datapath + line,  )
